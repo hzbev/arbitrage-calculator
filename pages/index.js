7451e3ba708cda2f58161d57eace90b86b2bc803
Ap0mano <apomanko1@gmail.com>
2023-03-30 12:08:03 +0200

    added 3way

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { binance } from 'ccxt';
 let binancee = new binance()
+import _2Way from "../components/2way.js"
+import _3Way from "../components/3way.js"
+
 
 export default function Home() {
-  const [odd1, setOdd1] = useState(0);
-  const [odd2, setOdd2] = useState(0);
-  const [arbPer, setArbPer] = useState(0);
-  const [totalStake, setTotalStake] = useState(0);
-
-
+  const [active, setActive] = useState(1);
   const [usdEur, setUsdEur] = useState(0);
 
 
   useEffect(() => {
-    if (odd1 > 0 && odd2 > 0) {
-      setArbPer(calArbPer(odd1, odd2))
-    }
     if (usdEur == 0) {
       (async () => {
         let { ask } = await binancee.fetchTicker('EUR/USDT')
@@ -23,103 +18,25 @@ export default function Home() {
         console.log("got price", ask)
       })()
     }
-  }, [odd1, odd2, totalStake, usdEur]);
+  }, [usdEur]);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    if (name === 'odd1') {
-      setOdd1(Number(value).toFixed(3));
-    }
-    if (name === 'odd2') {
-      setOdd2(Number(value).toFixed(3));
-    }
-  };
 
-  function copyVal(event) {
-    const { innerHTML } = event.target;
-    navigator.clipboard.writeText(innerHTML)
-  }
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-700">
-      <div className="h-1/2 sm:h-1/3 grid grid-cols-4 gap-3 p-5 rounded-lg bg-gray-600">
-        <div className="w-full h-full flex flex-col justify-center text-center font-normal text-xs text-gray-200">
-          <div>apple#3333</div>
-          <div>{usdEur}</div>
+      <div className='bg-gray-600 rounded-lg'>
+        <div className='h-10 sm:h-10 w-full flex justify-around align-middle rounded-lg bg-gray-400'>
+          <div onClick={e => setActive(1)} className={`${active == 1 ? "bg-gray-600 rounded-tr-lg" : ""} w-full bg-gray-400 text-center h-full rounded-tl-lg flex flex-col justify-center`}>2way</div>
+          <div onClick={e => setActive(2)} className={`${active == 2 ? "bg-gray-600 rounded-tl-lg rounded-tr-lg" : ""} w-full bg-gray-400 text-center h-full flex flex-col justify-center`}>3way</div>
+          <div onClick={e => setActive(3)} className={`${active == 3 ? "bg-gray-600 rounded-tl-lg" : ""} w-full bg-gray-400 text-center h-full rounded-tr-lg flex flex-col justify-center`}>convert</div>
+
         </div>
-        <div className="w-full h-full flex flex-col justify-center text-center font-bold text-xl text-gray-200">Stake</div>
-        <div className="w-full h-full flex flex-col justify-center text-center font-bold text-xl text-gray-200">USD</div>
-        <div className="w-full h-full flex flex-col justify-center text-center font-bold text-xl text-gray-200">EUR</div>
-        <div className="w-full h-full">
-          <input className='bg-gray-400 text-center w-full h-full placeholder-gray-700 font-medium'
-            type="number"
-            name='odd1'
-            placeholder='Odd 1'
-            onInput={e => setOdd1(e.target.value)}
-          />
-        </div>
-        <div className="bg-gray-500 flex flex-col w-full h-full text-center justify-center text-gray-300 font-semibold" onClick={copyVal}>{(calStakes(totalStake, arbPer.firstOddPer, arbPer.total) || 0).toFixed(2)}</div>
-        <div className="bg-gray-500 flex flex-col w-full h-full text-center justify-center text-gray-300 font-semibold" onClick={copyVal}>{(calStakes(totalStake, arbPer.firstOddPer, arbPer.total) * usdEur || 0).toFixed(2)}</div>
-        <div className="bg-gray-500 flex flex-col w-full h-full text-center justify-center text-gray-300 font-semibold" onClick={copyVal}>{(calStakes(totalStake, arbPer.firstOddPer, arbPer.total) / usdEur || 0).toFixed(2)}</div>
-        <div className="w-full h-full">
-          <input className='bg-gray-400 text-center w-full h-full placeholder-gray-700 font-medium'
-            type="number"
-            name='odd2'
-            placeholder='Odd 2'
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="bg-gray-500 flex flex-col w-full h-full text-center justify-center text-gray-300 font-semibold" onClick={copyVal}>{(calStakes(totalStake, arbPer.secondOddPer, arbPer.total) || 0).toFixed(2)}</div>
-        <div className="bg-gray-500 flex flex-col w-full h-full text-center justify-center text-gray-300 font-semibold" onClick={copyVal}>{(calStakes(totalStake, arbPer.secondOddPer, arbPer.total) * usdEur || 0).toFixed(2)}</div>
-        <div className="bg-gray-500 flex flex-col w-full h-full text-center justify-center text-gray-300 font-semibold" onClick={copyVal}>{(calStakes(totalStake, arbPer.secondOddPer, arbPer.total) / usdEur || 0).toFixed(2)}</div>
-        <div className="w-full h-full text-center">
-          <input className='bg-gray-400 text-center w-full h-full placeholder-gray-700 font-medium'
-            type="number"
-            placeholder='Stake'
-            onChange={e => setTotalStake(e.target.value)}
-          />
-        </div>
-        <div className="w-full h-full text-center flex flex-col justify-center text-gray-300">
-          <div>
-            Payout:&nbsp;
-            <div className={`font-bold ${arbPer.total > 100 ? "text-red-400" : "text-green-400"}`}>
-              {((totalStake / (arbPer.total / 100)) || 0).toFixed(2)}
-            </div>
-          </div>
-        </div>
-        <div className="w-full h-full text-center flex flex-col justify-center text-gray-300">
-          <div>
-            Profit:&nbsp;
-            <div className={`font-bold ${arbPer.total > 100 ? "text-red-400" : "text-green-400"}`}>
-              {((totalStake / (arbPer.total / 100)) - totalStake || 0).toFixed(2)}
-            </div>
-          </div>
-        </div>
-        <div className="w-full h-full text-center flex flex-col justify-center text-gray-300">
-          <div>
-            ROI:&nbsp;
-            <div className={`font-bold ${arbPer.total > 100 ? "text-red-400" : "text-green-400"}`}>
-              {((totalStake / (arbPer.total / 100) * 100) / totalStake - 100 || 0).toFixed(2)}%
-            </div>
-          </div>
-        </div>
+        {active == 1 && <_2Way price={usdEur} />}
+        {active == 2 && <_3Way price={usdEur} />}
+
       </div>
     </div>
   )
 }
 
 
-function calStakes(stake, oddPer, arbper) {
-  return Math.round(((stake * oddPer) / arbper) * 100) / 100
-}
-
-// function ca
-function calArbPer(firstOdd, secondOdd) {
-  return {
-    "firstOdd": firstOdd,
-    "firstOddPer": ((1 / firstOdd) * 100).toFixed(6),
-    "secondOdd": secondOdd,
-    "secondOddPer": ((1 / secondOdd) * 100).toFixed(6),
-    "total": ((1 / firstOdd) * 100 + (1 / secondOdd) * 100).toFixed(6)
-  }
-}
